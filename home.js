f68459a648292b0db340451147cfaed9d40673c3
KUSHAL <81311270+KUSHAL1912@users.noreply.github.com>
2022-01-23 01:02:56 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,28 @@
+
+function playaudio(){
+  var v = document.getElementById('music-1');
+  
+  v.play();
+}
+function pauseaudio(){
+
+  var x=  document.getElementById('music-1');
+  x.pause();
+}
+function lightmode() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+}
+function darkmode(){
+  var ele = document.body;
+  ele.classList.toggle("dark-mode");
+}
+function openwin(){
+
+  window.alert("Our app is in developing stage we will soon bring it. 😊"); //&#128512));
+
+}
+function nextsong(){
+
+ 
+}
